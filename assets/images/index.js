ddfd6543d757f1f3924a00169e5d08eae0453d7b
Mr Speaker <mrspeaker@gmail.com>
2015-11-28 13:27:33 -0500

    some more assets

--- assets/images/index.js
+++ assets/images/index.js
@@ -1,8 +1,17 @@
 // This should be magically generated
 export default [
+  "boxEmpty.png",
+  "grass.png",
+  "grassCenter_rounded.png",
+  "grassHalf.png",
+  "ladder_mid.png",
+  "ladder_top.png",
+  "liquidWater.png",
   "p1_jump.png",
   "p1_stand.png",
   "p3_duck.png",
+  "signExit.png",
   "spaceMonster1.png",
   "spaceship1.png",
+  "tochLit.png",
 ];
